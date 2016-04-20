86598ef843ced9fc6ab7904455df774413436928
max blessen <maxblessen@gmail.com>
2016-04-20 15:40:39 -0500

    Initial Commit

---
+++ CodeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodeTest
+//
+//  Created by max blessen on 4/20/16.
+//  Copyright © 2016 IdeaHouse LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodeTest
+//
+//  Created by max blessen on 4/20/16.
+//  Copyright © 2016 IdeaHouse LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
